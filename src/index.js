71cd27de617932971f68d05e9f7cf640d71bbaad
Megape4enka <mega.pe4enka@gmail.com>
2019-03-17 00:21:37 +0300

    add

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    counter = 0;
+    i = 0 ;
+    for (i = 0; i < preferences.length; i++) {
+      el = preferences[i];
+      el (el > preferences. length) {
+        continue;
+      }
+       elNext = preferences[el-1];
+      if (elNext == 1+i) {
+        continue;
+      }
+    return counter;
 };
